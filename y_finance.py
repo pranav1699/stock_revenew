d9cc792397d89820f28f26b7531700b29eb398af
pranav1699 <pranavsadagopan@gmail.com>
2021-12-21 21:48:58 +0530

    First commit

---
+++ y_finance.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import yfinance as yf
+import json
+import streamlit as st
+from numerize import numerize
+
+
+@st.cache
+def data_csv(filename):
+    data = pd.read_csv(filename)
+    data1 = data.Symbol
+    symbol_list = data1.tolist()
+    return symbol_list
+
+
+def main():
+    datas = data_csv("nasdaq_screener_1640095939797.csv")
+
+    options = st.selectbox("Select the Symbol", datas)
+
+    fb = yf.Ticker(options)
+
+    name = fb.info['shortName']
+    revenue = numerize.numerize(fb.info['totalRevenue'])
+    revenue_increase = fb.info['revenueGrowth']
+    summary = fb.info['longBusinessSummary']
+    total_cash = numerize.numerize(fb.info['totalCash'])
+    totalCashPerShare = numerize.numerize(fb.info['totalCashPerShare'])
+    revenue_percent = f"{revenue_increase} %"
+
+    image = fb.info['logo_url']
+    st.header("Revenue")
+    st.image(image, width=50)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(name, f"${revenue}", revenue_percent)
+        # st.write(fb.info)
+    with col2:
+        st.write("Total Cash : ", '$', total_cash)
+        st.write("Total Cash Per Share : ", '$', totalCashPerShare)
+    
+    with st.expander("Description"):
+        st.write(summary)
+
+
+hide_streamlit_style = """
+                        <style>
+                        #MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}
+                        border-top:5px
+                        </style>
+                        """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
